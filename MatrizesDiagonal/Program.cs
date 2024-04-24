6b62454827688e451aa25e151e70a74451fdb43b
cauematheus23 <cauematheus_23@hotmail.com>
2024-04-24 14:19:21 -0300

    Add project files.

---
+++ MatrizesDiagonal/Program.cs
@@ -0,0 +1,63 @@
+﻿int qtdlinha = 3, qtdcoluna = 3, diagonal = 0, soma_linha = 0,soma_coluna = 0;
+int[,] matriz = new int[qtdlinha, qtdcoluna];
+for (int linha = 0; linha < qtdlinha; linha++)
+{
+    for (int coluna = 0; coluna < qtdcoluna; coluna++)
+    {
+        matriz[linha, coluna] = new Random().Next(1, 11);
+
+    }
+}
+for (int linha = 0; linha < qtdlinha; linha++)
+{
+    for (int coluna = 0; coluna < qtdcoluna; coluna++)
+    {
+        Console.Write(matriz[linha,coluna] + " ");
+
+
+    }
+    Console.WriteLine();
+}
+
+for (int linha = 0; linha < qtdlinha; linha++)
+{
+    soma_coluna = 0;
+    for (int coluna = 0; coluna < qtdcoluna; coluna++)
+    {
+     soma_coluna += matriz[linha, coluna];
+    }
+                Console.WriteLine($"Linha {linha} soma = {soma_coluna}");
+    }
+
+for (int coluna = 0; coluna < qtdcoluna; coluna++) { 
+    soma_linha = 0;
+    for (int linha = 0; linha < qtdlinha; linha++)
+    {
+        soma_linha += matriz[linha, coluna];
+    } Console.WriteLine($"coluna {coluna} soma = " + soma_linha);
+}
+for (int linha = 0; linha < qtdlinha; linha++)
+{
+    for (int coluna = 0; coluna < qtdcoluna; coluna++)
+    {
+        if (coluna == linha)
+        {
+            diagonal += matriz[linha, coluna];
+            continue;
+        }
+    }
+}
+        Console.WriteLine("Diagonal soma = " + diagonal);
+diagonal = 0;
+for (int linha = 0; linha < qtdlinha; linha++)
+{
+    for (int coluna = 0; coluna <qtdcoluna; coluna++)
+    {
+        if (linha + coluna ==  qtdlinha - 1)
+        {
+            diagonal += matriz[linha, coluna];
+            continue;
+        }
+    }
+}
+        Console.WriteLine("Diagonal soma = " + diagonal);
